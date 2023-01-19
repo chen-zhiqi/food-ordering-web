3e0bec6cd9db122b1c5ae4ac0b062b1184eefed3
jeremiahyeap <jeremiahyeap@gmail.com>
2023-01-19 14:16:28 +0800

    Added jUnit for review

---
+++ food_order/src/main/java/com/dvops/maven/eclipse/Review.java
@@ -0,0 +1,26 @@
+package com.dvops.maven.eclipse;
+
+public class Review {
+	
+	protected String review;
+
+	/**
+	 * @return the review
+	 */
+	public String getReview() {
+		return review;
+	}
+
+	/**
+	 * @param review the review to set
+	 */
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public Review(String review) {
+		super();
+		this.review = review;
+	}
+
+}
