b49383dcaa2b1220481e133f79ff7ac9027793df
chen-zhiqi <2102901B@student.tp.edu.sg>
2023-02-02 11:40:28 +0800

    created registerTest.java file for JUnit testing

---
+++ food_order/src/test/java/com/dvops/maven/eclipse/RegisterTest.java
@@ -0,0 +1,19 @@
+package com.dvops.maven.eclipse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegisterTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testdoPost() {
+		fail("Not yet implemented");
+	}
+
+}
