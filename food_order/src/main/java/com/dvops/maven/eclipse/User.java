5b099736298f3035bb6247fd69330b8190595ecc
chen-zhiqi <2102901B@student.tp.edu.sg>
2023-02-02 16:37:43 +0800

    edit comments out on user.java file

--- food_order/src/main/java/com/dvops/maven/eclipse/User.java
+++ food_order/src/main/java/com/dvops/maven/eclipse/User.java
@@ -1,57 +1,35 @@
 package com.dvops.maven.eclipse;
 
-public class user{
+public class User{
 	private String n;
 	private String e;
 	private String p;
 	
-	/**
-	 * @param n
-	 * @param e
-	 * @param p
-	 */
-	public user(String n, String e, String p) {
+	public User(String n, String e, String p) {
 		super();
 		this.n = n;
 		this.e = e;
 		this.p = p;
 	}
 	
-	/**
-	 * @return the user
-	 */
 	public String getName() {
 		return n;
 	}
-	/**
-	 * @param n the name to set
-	 */
 	public void setName(String n) {
 		this.n = n;
 	}
 
-	/**
-	 * @return the email
-	 */
 	public String getEmail() {
 		return e;
 	}
-	/**
-	 * @param e the email to set
-	 */
 	public void setEmail(String e) {
 		this.e = e;
 	}
 	
-	/**
-	 * @return the password
-	 */
+
 	public String getPassword() {
 		return p;
 	}
-	/**
-	 * @param p the password to set
-	 */
 	public void setPassword(String p) {
 		this.p = p;
 	}
